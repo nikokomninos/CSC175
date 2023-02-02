3ee586d44c07227f43c83f04710775530ef358ed
Niko Komninos <nikokomninos@gmail.com>
2023-02-02 16:14:52 -0500

    Commit

--- Lab03/ForceConverter.java
+++ Lab03/ForceConverter.java
@@ -9,28 +9,39 @@ import java.util.*;
 
 public class ForceConverter{
     public static void main(String[] args) {
+        double poundForce = readLbf();
+        double convertedPoundForce = convert(poundForce);
+        printConversion(poundForce, convertedPoundForce);
+
+    }
+
+    /*readLbf()- recieves value in pound-force to be converted to newtons from user 
+    Parameters - N/A 
+    */
+    public static double readLbf(){
         //Initalize scanner object, declare variables, constants
         Scanner scan = new Scanner(System.in);
 
         //Prompt the user to input a value in pound-force
         System.out.println("Please enter a value in pound-force to convert to newtons: ");
-
-        //Pass user input as parameter to convert method
-        convert(scan.nextDouble());
-        scan.close();
-
+        return scan.nextDouble();
     }
 
     /*convert()- converts pound-force to newtons through multiplying by the newton constant
     Parameters - poundForce, which represents the value that the user would like to convert to Newtons 
     Precondition: poundForce is a non-negative floating point number
     */
-    public static void convert(double poundForce){
+    public static double convert(double poundForce){
         final double NEWTON = 4.44822162; //constant
-        double convertedPoundForce = NEWTON * poundForce; //conversion
+        return NEWTON * poundForce; //return conversion
+    }
 
+    /*printConversion()- prints the original and converted values of weight 
+    Parameters - poundForce, the original value, and convertedPoundForce, the converted value 
+    Precondition: poundForce and convertedPoundForce are non-negative floating point numbers
+    */
+    public static void printConversion(double poundForce, double convertedPoundForce){
         //Print the original and converted value
         System.out.println("\nValue (Pound-force): " + poundForce + "\nValue (Newtons): " + convertedPoundForce);
-
     }
 }
--- Lab03/TempConverter.java
+++ Lab03/TempConverter.java
@@ -14,32 +14,45 @@ public class TempConverter
    //-----------------------------------------------------------------
    public static void main (String[] args)
    {
-      // Declare variables.
-      Scanner scan = new Scanner(System.in);
-
-      //Prompt user to input temperature in celsius
-      System.out.println("Please enter a temperature in degrees Celsius: ");
-      //Pass user-inputted temp as parameter for convert method
-      convert(scan.nextInt());
-      scan.close();
+      int celsiusTemp = readCelsius();
+      double fahrenheitTemp = convert(celsiusTemp);
+      printConversion(celsiusTemp, fahrenheitTemp);
 
 
    } // end main
+
+    /*readCelsius()- recieves value in celsius to be converted to fahrenheit from user
+    Parameters - N/A 
+    */
+   public static int readCelsius(){
+      Scanner scan = new Scanner(System.in);
+      //Prompt user to input temperature in celsius
+      System.out.println("Please enter a temperature in degrees Celsius: ");
+      return scan.nextInt();
+
+   }
 
     /*convert()- converts celsius to farenhiet through multiplying by the conversion factor and adding the base difference 
     Parameters - celsiusTemp, which represents the value that the user would like to convert to farenheit
     Precondition - celsiusTemp is a non-negative integer 
     */
-   public static void convert(int celsiusTemp){
+   public static double convert(int celsiusTemp){
       final double CONVERSION_FACTOR = (9.0/5.0); //conversion factor between celsius and farenheit
       final int BASE = 32; //base difference between celsius and farenheit
 
       // Compute temperature in fahrenheit.
-      double fahrenheitTemp = (celsiusTemp * CONVERSION_FACTOR) + BASE;
+      return (celsiusTemp * CONVERSION_FACTOR) + BASE;
 
+   }
+
+    /*printConversion()- prints the original and converted temperature values
+    Parameters - celsiusTemp, which represents the value that the user would like to convert to farenheit, and fahrenheitTemp, the converted value
+    Precondition - celsiusTemp and fahrenheitTemp are non-negative integers
+    */
+   public static void printConversion(int celsiusTemp, double fahrenheitTemp){
       // Print equivalent temperatures in celsius and fahrenheit.
       System.out.println ("\nCelsius Temperature: " + celsiusTemp);
       System.out.println ("Fahrenheit Equivalent: " + fahrenheitTemp);
-   }
 
+   }
 } // end TempConverter
